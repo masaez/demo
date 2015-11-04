2389d6189447c21fa5d78cacb0b504a02d5a1b34
Miguel Angel Saez <miguel@autocosmos.com>
2015-11-04 19:53:28 -0300

    Saludando al mug

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Llego request de ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hola Code y Azure Web Apps!</h1>');
+    res.end('<h1>Hola MUG!!!</h1>');
 }).listen(process.env.PORT);
